0fc59b882b102d9a4ef06b0ebcbeabdc73ee854e
CoulsonP <coulsonp@icloud.com>
2022-10-30 16:47:44 -0500

    added options cart

--- Develop/script.js
+++ Develop/script.js
@@ -20,22 +20,34 @@ if (passwordlength < 8 || passwordlength > 128) {
 // Prompts user to ask what type of characters they want in there password
 var userWantsNumbers = window.confirm("Do you want to have numbers in your password?")
 var userWantsUppercase = window.confirm("Do you want to have uppercase letters in your password?")
-var userWantslowercase = window.confirm("Do you want to have lowercase letters in your password?")
-var userWantssymbols = window.confirm("Do you want to have symbols in your password?")
+var userWantsLowercase = window.confirm("Do you want to have lowercase letters in your password?")
+var userWantsSymbols = window.confirm("Do you want to have symbols in your password?")
 // array lists for prompt drop down
 var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var symbolist = ["!", '@', "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+", ".", ",", ";", ":", "?", "'", "<", ">"]
 var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-}
+
 
 var optionsCart = []
 
 if (userWantsNumbers === true) {
-    optionsCart.push()
+    optionsCart.push(numberList)
 }
 
+if (userWantsSymbols === true) {
+    optionsCart.push(symbolist)
+}
 
+if (userWantsLowercase === true) {
+    optionsCart.push(lowercaseList)
+}
+
+if (userWantsUppercase === true) {
+    optionsCart.push(uppercaseList)
+}
+
+}
 
 // Write password to the #password input
 function writePassword() {
